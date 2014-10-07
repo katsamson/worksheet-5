df7435b78672a8159302f39851de806589cf4465
Ann Schulte/Katie Samson <aschulte@nebrwesleyan.edu>
2014-10-07 11:06:59 -0400

    added 3 and 4.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 function flip_coin (){
  var result;
-if (Math.random <= 0.5){
+if (Math.random() <= 0.5){
   result = 'heads';
 } else {
   result = 'tails';
@@ -15,25 +15,44 @@ if (Math.random <= 0.5){
 
 function roll_die (){
  var result;
-if (Math.random <= .1667){
+ var random_number;
+ random_number = Math.random();
+ if (random_number <= .1667){
   result = 1;
   }
-  	if (Math.random <= .1667*2){
+  	if (random_number <= .1667*2){
 		result = 2;
 		}
-			if (Math.random <= .1667*3){
-				result = 3;
-				}
-				if (Math.random <= .1667*4){
-					result = 4;
-					}
-					if (Math.random <= .1667*5){
-						result = 5;
-						}
-						if (Math.random <= .1667*6){
-							result = 6;
-							}
+	if (random_number <= .1667*3){
+		result = 3;
+		}
+	if (random_number <= .1667*4){
+		result = 4;
+		}
+	if (random_number <= .1667*5){
+		result = 5;
+		}
+	if (random_number <= .1667*6){
+		result = 6;
+		}
   return result;
 }
 
+function 5rolls() {
+    var result = [0];
+    var index = 0
+    while (index < 5) {
+        result[index] = roll_die();
+        index = index + 1
+    }
+    return result;
+}
+
+function yahtzee() {
+    var result[index];
+    if (result[0] === result[1] && result[1] === result[2] && result[2] === result[3] && result[3] === result[4]) {
+        return 50;
+    }
+    return 0;
+}
 
